b893ad388ce698fe467dd995e53f4aa7b5e7f4b0
cuddiale <alessiocuddretto@hotmail.com>
2024-06-03 18:16:48 +0200

    Update

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+const animationData = [
+  {
+    inputVal: 5,
+    marginTop: 300,
+    addElDelay: 1000,
+  },
+  {
+    inputVal: 2,
+    marginTop: -200,
+    addElDelay: 1500,
+  },
+  {
+    inputVal: 1,
+    marginTop: -200,
+    addElDelay: 2000,
+  },
+];
 
 const decimalToBinary = (input) => {
   if (input === 0 || input === 1) {
@@ -11,15 +29,15 @@ const decimalToBinary = (input) => {
 };
 
 const showAnimation = () => {
-  setTimeout(() => {
-    console.log("free");
-  }, 500);
-  setTimeout(() => {
-    console.log("Code");
-  }, 1000);
-  setTimeout(() => {
-    console.log("Camp");
-  }, 1500);
+  result.innerText = "Call Stack Animation";
+
+  animationData.forEach((obj) => {
+    setTimeout(() => {
+      animationContainer.innerHTML += `
+        <p id=""></p>
+      `;
+    }, obj.addElDelay);
+  });
 };
 
 const checkUserInput = () => {
