e07930da80739f15224c0b0055c9541b1112a18f
cuddiale <alessiocuddretto@hotmail.com>
2024-06-03 09:31:16 +0200

    Update

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const checkUserInput = () => {};
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+  }
+
+  console.log(numberInput.value);
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
