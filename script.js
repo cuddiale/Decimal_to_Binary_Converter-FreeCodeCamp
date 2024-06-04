2ee81a8138dff53273c5e85779906d3daf20c1d4
cuddiale <alessiocuddretto@hotmail.com>
2024-06-04 09:29:20 +0200

    Update

--- script.js
+++ script.js
@@ -12,11 +12,17 @@ const animationData = [
     inputVal: 2,
     marginTop: -200,
     addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
   },
   {
     inputVal: 1,
     marginTop: -200,
     addElDelay: 2000,
+    msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
   },
 ];
 
@@ -34,7 +40,9 @@ const showAnimation = () => {
   animationData.forEach((obj) => {
     setTimeout(() => {
       animationContainer.innerHTML += `
-        <p id="${obj.inputVal}"></p>
+        <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
+          decimalToBinary(${obj.inputVal})
+        </p>
       `;
     }, obj.addElDelay);
   });
